a83e8825ac0264f44fb0d04fcf13293c3c6e6fd8
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-11-25 20:20:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/araccessories/ui/features/splashScreen/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.example.araccessories.ui.features.splashScreen
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.araccessories.R
+import com.example.araccessories.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+
+            view?.findNavController()?.navigate(R.id.action_splashFragment_to_onBoardingFragment)
+        }, 4500)
+        return binding.root
+    }
+
+
+}
