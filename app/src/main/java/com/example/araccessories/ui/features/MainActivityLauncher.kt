4b4bf8938c9a079a9b9f530041033e0a2bc2685d
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-11-26 23:33:40 +0200

    Splash Screen

--- app/src/main/java/com/example/araccessories/ui/features/MainActivityLauncher.kt
+++ app/src/main/java/com/example/araccessories/ui/features/MainActivityLauncher.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.araccessories.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivityLauncher : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
