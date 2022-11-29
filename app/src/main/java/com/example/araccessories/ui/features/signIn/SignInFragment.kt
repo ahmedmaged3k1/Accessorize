63bd26affaa909dfaf7d14d0b81dbdde2b6503d6
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-11-29 23:08:33 +0200

    Onboarding check point

---
+++ app/src/main/java/com/example/araccessories/ui/features/signIn/SignInFragment.kt
@@ -0,0 +1,29 @@
+package com.example.araccessories.ui.features.signIn
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.araccessories.R
+import com.example.araccessories.databinding.FragmentSignInBinding
+import com.example.araccessories.databinding.FragmentSplashBinding
+
+
+class SignInFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignInBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+}
