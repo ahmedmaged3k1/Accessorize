2af4a967db51355f259f1157d1ca63bbe01031ab
Ahmed Maged <60134186+ahmedmaged3k1@users.noreply.github.com>
2023-04-25 02:01:12 +0200

    Glasses Try On Refactor

--- app/src/main/java/com/example/araccessories/ui/features/glassesTryOn/GlassesTryOn.kt
+++ app/src/main/java/com/example/araccessories/ui/features/glassesTryOn/GlassesTryOn.kt
@@ -1,147 +1,47 @@
 package com.example.araccessories.ui.features.glassesTryOn
 
-import android.annotation.SuppressLint
 import android.app.ActivityManager
-import android.content.ContentValues
 import android.content.Context
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.provider.MediaStore
 import android.view.LayoutInflater
-import android.view.PixelCopy
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.araccessories.R
 import com.example.araccessories.ui.core.utilities.GlassesActivity
-import com.google.ar.core.*
-import com.google.ar.sceneform.ArSceneView
-import com.google.ar.sceneform.Scene
-import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.Renderable
-import com.google.ar.sceneform.ux.*
-import kotlinx.android.synthetic.main.activity_glasses.*
-import kotlinx.android.synthetic.main.fragment_glasses_try_on.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.File
-import java.io.FileOutputStream
-import java.util.*
+import com.google.ar.core.ArCoreApk
+import com.google.ar.core.AugmentedFace
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.AugmentedFaceNode
 
 
-class GlassesTryOn :Fragment()   {
-    private var isDepthSupported = false
+class GlassesTryOn : Fragment() {
     private val args by navArgs<GlassesTryOnArgs>()
     private lateinit var arFragment: ArFragment
-    private lateinit var sceneView: ArSceneView
-    private lateinit var scene: Scene
-    private lateinit var config: Config
-    private var faceRegionsRenderable: ModelRenderable? = null
     var faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
+    private val viewModel: GlassesTryOnViewModel by viewModels()
+    private lateinit var captureShot: ImageButton
 
-    @SuppressLint("SuspiciousIndentation")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_glasses_try_on, container, false)
         if (!checkIsSupportedDeviceOrFinish()) {
-            //navigate
             view?.findNavController()?.navigate(R.id.action_glassesTryOn_to_productDetailsFragment)
         }
-        arFragment = childFragmentManager.findFragmentById(R.id.face_fragment_glasses) as? ArFragment ?: return view
-        configureArSession()
-        tryOnProduct()
-        //moveModel()
+        arFragment =
+            childFragmentManager.findFragmentById(R.id.face_fragment_glasses) as? ArFragment
+                ?: return view
+        captureShot = view.findViewById(R.id.captureImage)
+        viewModel.tryOnProduct(args.product.productModel, arFragment)
+        takeSnapShot()
         return view
-    }
-    private fun configureArSession(){
-        sceneView = arFragment.arSceneView
-        sceneView.cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
-        scene = sceneView.scene
-        faceRegionsRenderable=args.product.productModel
-    }
-    private fun enableDepth(){
-        config = sceneView.session!!.config
-        isDepthSupported =
-            sceneView.session!!.isDepthModeSupported(Config.DepthMode.AUTOMATIC)
-        if (isDepthSupported) {
-            config.setDepthMode(Config.DepthMode.AUTOMATIC)
-        } else {
-            config.setDepthMode(Config.DepthMode.DISABLED)
-        }
-        sceneView.session!!.configure(config)
-
-    }
-    private fun tryOnProduct() {
-        scene.addOnUpdateListener {
-            if (faceRegionsRenderable != null) {
-                sceneView.session
-                    ?.getAllTrackables(AugmentedFace::class.java)?.let {
-                        enableDepth()
-                        for (face in it) {
-                            if (!faceNodeMap.containsKey(face)) {
-                             attachModel(face)
-                                CoroutineScope(Dispatchers.IO).launch{
-                                    takeSnapShot()
-                                }
-                            }
-                        }
-                  removeRedundantModels()
-                    }
-            }
-        }
-    }
-    private fun attachModel(face : AugmentedFace){
-        val faceNode = AugmentedFaceNode(face)
-        faceNode.setParent(scene)
-        face.getRegionPose(AugmentedFace.RegionType.NOSE_TIP)
-        faceNode.faceRegionsRenderable = faceRegionsRenderable
-        faceNodeMap[face] = faceNode
-    }
-    private fun removeRedundantModels(){
-        // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
-        val iter = faceNodeMap.entries.iterator()
-        while (iter.hasNext()) {
-            val entry = iter.next()
-            val face = entry.key
-            if (face.trackingState == TrackingState.STOPPED) {
-                val faceNode = entry.value
-                faceNode.setParent(null)
-                iter.remove()
-            }
-        }
-    }
-    private fun takeSnapShot(){
-        captureImage.setOnClickListener{
-            val width =  sceneView.width
-            val height =  sceneView.height
-            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-            PixelCopy.request(sceneView,bitmap,{res ->
-                val filename = "${args.product.productName} Try On_${System.currentTimeMillis()}.png"
-                val contentValues = ContentValues().apply {
-                    put(MediaStore.Images.Media.DISPLAY_NAME, filename)
-                    put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-                    put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
-                }
-                val contentResolver = requireContext().contentResolver
-                val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-                uri?.let {
-                    contentResolver.openOutputStream(it).use { outputStream ->
-                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-                        Toast.makeText(requireContext(), "Photo saved to gallery", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }, Handler(Looper.getMainLooper()))
-
-
-        }
     }
 
 
@@ -159,11 +59,11 @@ class GlassesTryOn :Fragment()   {
                 ?.deviceConfigurationInfo
                 ?.glEsVersion
 
-        openGlVersionString?.let { s ->
+        openGlVersionString?.let {
             if (java.lang.Double.parseDouble(openGlVersionString) < GlassesActivity.MIN_OPENGL_VERSION) {
                 Toast.makeText(
                     this.context,
-                    "Sceneform requires OpenGL ES 3.0 or later",
+                    "Scene-form requires OpenGL ES 3.0 or later",
                     Toast.LENGTH_LONG
                 )
                     .show()
@@ -173,19 +73,15 @@ class GlassesTryOn :Fragment()   {
         }
         return true
     }
-    /*   private fun moveModel(){
-           arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-               val anchor = hitResult.createAnchor()
-               val anchorNode = AnchorNode(anchor)
-               anchorNode.setParent(arFragment.arSceneView.scene)
-               val node = TransformableNode(arFragment.transformationSystem)
-               node.renderable = faceRegionsRenderable
-               node.setParent(anchorNode)
-               arFragment.arSceneView.scene.addChild(anchorNode)
-               node.select()
-           }
-       }*/
 
+
+    private fun takeSnapShot() {
+        captureShot.setOnClickListener {
+            viewModel.takeSnapShot(requireContext())
+        }
+
+
+    }
 
 
 }
--- app/src/main/java/com/example/araccessories/ui/features/glassesTryOn/GlassesTryOnViewModel.kt
+++ app/src/main/java/com/example/araccessories/ui/features/glassesTryOn/GlassesTryOnViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.araccessories.ui.features.glassesTryOn
 
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
+import android.view.PixelCopy
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.navArgs
+import androidx.lifecycle.viewModelScope
 import com.google.ar.core.AugmentedFace
 import com.google.ar.core.Config
 import com.google.ar.core.TrackingState
@@ -11,20 +19,16 @@ import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.AugmentedFaceNode
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.HashMap
 
 class GlassesTryOnViewModel :ViewModel() {
     private var isDepthSupported = false
-
     private lateinit var sceneView: ArSceneView
     private lateinit var scene: Scene
     private lateinit var config: Config
     private var faceRegionsRenderable: ModelRenderable? = null
     var faceNodeMap = HashMap<AugmentedFace, AugmentedFaceNode>()
-    private fun configureArSession(productModel : ModelRenderable ,arFragment: ArFragment  ){
+    private fun configureArSession(productModel : ModelRenderable? ,arFragment: ArFragment  ){
         sceneView = arFragment.arSceneView
         sceneView.cameraStreamRenderPriority = Renderable.RENDER_PRIORITY_FIRST
         scene = sceneView.scene
@@ -42,7 +46,8 @@ class GlassesTryOnViewModel :ViewModel() {
         sceneView.session!!.configure(config)
 
     }
-    private fun tryOnProduct() {
+     fun tryOnProduct(productModel: ModelRenderable?, arFragment: ArFragment) {
+        configureArSession(productModel, arFragment)
         scene.addOnUpdateListener {
             if (faceRegionsRenderable != null) {
                 sceneView.session
@@ -51,9 +56,7 @@ class GlassesTryOnViewModel :ViewModel() {
                         for (face in it) {
                             if (!faceNodeMap.containsKey(face)) {
                                 attachModel(face)
-                                CoroutineScope(Dispatchers.IO).launch{
-                                    //takeSnapShot()
-                                }
+
                             }
                         }
                         removeRedundantModels()
@@ -70,17 +73,46 @@ class GlassesTryOnViewModel :ViewModel() {
     }
     private fun removeRedundantModels(){
         // Remove any AugmentedFaceNodes associated with an AugmentedFace that stopped tracking.
-        val iter = faceNodeMap.entries.iterator()
-        while (iter.hasNext()) {
-            val entry = iter.next()
+        val inter = faceNodeMap.entries.iterator()
+        while (inter.hasNext()) {
+            val entry = inter.next()
             val face = entry.key
             if (face.trackingState == TrackingState.STOPPED) {
                 val faceNode = entry.value
                 faceNode.setParent(null)
-                iter.remove()
+                inter.remove()
             }
         }
     }
+     fun takeSnapShot(context : Context){
+        viewModelScope.launch {
+            val width =  sceneView.width
+            val height =  sceneView.height
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            PixelCopy.request(sceneView,bitmap,{ res ->
+                val filename = "Try On_${System.currentTimeMillis()}.png"
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+                    put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+                    put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+                }
+                val contentResolver = context.contentResolver
+                val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                uri?.let {
+                    contentResolver.openOutputStream(it).use { outputStream ->
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                        Toast.makeText(context, "Photo saved to gallery", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }, Handler(Looper.getMainLooper()))
+
+
+        }
+
+
+    }
+
+
 
 
 }
