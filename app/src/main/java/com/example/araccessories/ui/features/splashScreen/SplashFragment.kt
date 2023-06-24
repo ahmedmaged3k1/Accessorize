0b70cc68587c410e4dc156130603eacc3830a7b4
Ahmed Maged <60134186+ahmedmaged3k1@users.noreply.github.com>
2023-06-24 05:36:35 +0300

    Splash Screen

--- app/src/main/java/com/example/araccessories/ui/features/splashScreen/SplashFragment.kt
+++ app/src/main/java/com/example/araccessories/ui/features/splashScreen/SplashFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.navigation.findNavController
 import com.example.araccessories.R
 import com.example.araccessories.databinding.FragmentSplashBinding
@@ -20,13 +22,64 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSplashBinding.inflate(inflater, container, false)
 
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
+        activity?.window?.decorView?.apply {
+            systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
 
-            view?.findNavController()?.navigate(R.id.action_splashFragment_to_onBoardingFragment)
-        }, 2300)
+        activity?.window?.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        binding.motionLayout.startLayoutAnimation()
+        binding.motionLayout.setTransitionListener(object :MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                view?.findNavController()?.navigate(R.id.action_splashFragment_to_onBoardingFragment)
+
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+
+            }
+
+        })
+
+//        val handler = Handler(Looper.getMainLooper())
+//        handler.postDelayed({
+//
+//            view?.findNavController()?.navigate(R.id.action_splashFragment_to_onBoardingFragment)
+//        }, 2300)
         return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        // Clear the window flags
+        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
 
 }
